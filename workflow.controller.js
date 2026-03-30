486639bc6dcacd95bade5b3e16e4046b3bb3f697
Kecskés Gellért <gellertkecskes@gmail.com>
2026-03-30 22:07:12 +0200

    Add files via upload

--- workflow.controller.js
+++ workflow.controller.js
@@ -1,3 +1,5 @@
+import dayjs from "dayjs";
+import Subscription from "../models/subscrition.model.js";
 import {createRequire} from "module";
 const requires = createRequire(import.meta.url);
 const { serve } = require("@upstash/workflow/express");
@@ -6,10 +8,14 @@ export const   sendReminder = serve(async (context) =>{
     const {subscriptionId} = context.requestPayload;
     const subscription = await fetchSubscription(context, subscriptionId);
     if (!subscription || subscription.status !== active) return;
-    const renewalDate = new Date(subscription.renewalDate);
+    const renewalDate = dayjs(subscription.renewalDate);
+    if(renewalDate.isBefore(dayjs())) {
+        console.log(`Renewal date has passed for subscription ${subscriptionId}`);
+    }
 });
 const fetchSubscription = async (context, subscriptionId) => {
     return await context.run("Get subscription",() =>{
-        return Subscription.findById
+        return Subscription.findById(subscriptionId).populate("user","name email");
+
     })
 }
